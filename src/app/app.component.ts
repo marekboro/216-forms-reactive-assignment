ed7146dec841d23ee1b4e74c032de00d096cba27
marekboro <marek.borowicz83@gmail.com>
2021-11-06 13:02:51 +0000

    finished assignment, added the async validator and form valuse print to console upon submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
-
 
   projectForm: FormGroup
   projectStatuses = ['Stable', 'Critical', 'Finished']
@@ -19,7 +17,9 @@ export class AppComponent implements OnInit {
   ngOnInit() {
 
     this.projectForm = new FormGroup({
-      'projectName': new FormControl(null, [Validators.required, this.forbiddenProjectName]),
+
+      'projectName': new FormControl(null, Validators.required, this.forbiddenProjectName),
+      // 'projectName': new FormControl(null, Validators.required),
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'status': new FormControl('Stable')
     })
@@ -27,16 +27,31 @@ export class AppComponent implements OnInit {
   }
 
   onSubmit() {
-
+    console.log(this.projectForm.value)
   }
 
-  forbiddenProjectName(control: FormControl): { [s: string]: boolean } {
-    if (control.value === "Test") {
-      return { 'projectNameIsForbidden': true }
-    }
-    else {
-      return null
-    }
+  // forbiddenProjectName(control: FormControl): { [s: string]: boolean } {
+  //   if (control.value === "Test") {
+  //     return { 'projectNameIsForbidden': true }
+  //   }
+  //   else {
+  //     return null
+  //   }
+  // }
+
+  forbiddenProjectName(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === "Test") {
+          resolve({ 'projectNameIsForbidden': true })
+        }
+        else {
+          resolve(null)
+        }
+      }, 1000);
+    })
+    return promise;
   }
+
 
 }
