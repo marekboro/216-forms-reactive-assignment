9de58b786d3319708ff7b8847efdd5ee08f04032
marekboro <marek.borowicz83@gmail.com>
2021-11-06 12:07:17 +0000

    TS form completed and partsly created with HTML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+projectForm: FormGroup
+projectStatus = ['Stable', 'Critical','Finished']
+
+constructor(formBuilder:FormBuilder){}
+
+
+ngOnInit(){
+
+  this.projectForm = new FormGroup({ 
+    'projectName': new FormControl(null, Validators.required),
+    'email': new FormControl(null, [Validators.required, Validators.email]),
+    'status': new FormControl('Stable')
+  })
+
+}
+
+onSubmit(){
+
+}
+
 }
