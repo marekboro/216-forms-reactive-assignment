be2cf38315fe91343522065139c91fc351102133
marekboro <marek.borowicz83@gmail.com>
2021-11-06 12:23:39 +0000

    added validator to prevent Test being used as a project name, added css to look at input.ng-invalid.ng-touched to hightlight when this is the case.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,37 @@ import { FormBuilder, FormArray, FormControl, FormGroup, Validators } from '@ang
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-
-projectForm: FormGroup
-projectStatuses = ['Stable', 'Critical','Finished']
-
-constructor(formBuilder:FormBuilder){}
+export class AppComponent implements OnInit {
 
 
-ngOnInit(){
 
-  this.projectForm = new FormGroup({ 
-    'projectName': new FormControl(null, Validators.required),
-    'email': new FormControl(null, [Validators.required, Validators.email]),
-    'status': new FormControl('Stable')
-  })
+  projectForm: FormGroup
+  projectStatuses = ['Stable', 'Critical', 'Finished']
 
-}
+  constructor(formBuilder: FormBuilder) { }
 
-onSubmit(){
 
-}
+  ngOnInit() {
+
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, [Validators.required, this.forbiddenProjectName]),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'status': new FormControl('Stable')
+    })
+
+  }
+
+  onSubmit() {
+
+  }
+
+  forbiddenProjectName(control: FormControl): { [s: string]: boolean } {
+    if (control.value === "Test") {
+      return { 'projectNameIsForbidden': true }
+    }
+    else {
+      return null
+    }
+  }
 
 }
